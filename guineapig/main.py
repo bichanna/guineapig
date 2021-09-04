ae360ddf9f8253c30a17e3b8ba0f43aa8df949a3
bichanna <nobu.bichanna@gmail.com>
2021-09-04 09:55:31 -0500

    Could not access to  as root.

--- guineapig/main.py
+++ guineapig/main.py
@@ -1,10 +1,11 @@
 import click
-from prompt import Prompt
+from .prompt import Prompt
 import subprocess
 import mysql.connector
 from mysql.connector.errors import DatabaseError
 import sys
 import platform
+from mysql.connector import errorcode
 
 def guineapig_print(value):
 	print(f"GUINEAPIG🐹: {value}")
@@ -39,14 +40,35 @@ def setupdb():
 		cursor.execute("CREATE DATABASE guineapig_db")
 		guineapig_print("Database `guineapig_db` is created.")
 	except DatabaseError:
-		guineapig_print("Database `guineapig_db` already exits.")
+		guineapig_print("Database `guineapig_db` already exists.")
 
 
 
 @main.command()
 def shell():
-	cnx = mysql.connector.connect(user="root",database="")
-	Prompt().cmdloop()
+	# connect to the database if it can be connected
+	try:
+		cnx = mysql.connector.connect(user="root",database="guineapig_db")
+		Prompt().cmdloop()
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_BAD_DB_ERROR:
+			guineapig_print("Database `guineapig_db` does not exist.")
+			guineapig_print("Please run `guineapig setupdb.`")
+			sys.exit()
+
+	# use guineapig_db
+	try:
+		cursor.execute("USE guineapig_db")
+	except mysql.connector.Error as err:
+		if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+			guineapig_print("Could not access to `guineapig_db` as root.")
+		else:
+			guineapig_print("Database `guineapig_db` does not exist.")
+			guineapig_print("Please run `guineapig setupdb.`")
+		sys.exit()
+		
+
+
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = """
 
 # guineapig
 
-guineapig is a CLI for tracking pocket money for youngsters who do not yet have a bank account.
+guineapig is a CLI for tracking pocket money.
 
 ### Installation
 ```
