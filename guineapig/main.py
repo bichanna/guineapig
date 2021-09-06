a5fdbb316e6d1539af58569252c9fea9c8c0d101
bichanna <nobu.bichanna@gmail.com>
2021-09-05 23:31:24 -0500

    solve ModuleNotFoundError

--- guineapig/main.py
+++ guineapig/main.py
@@ -1,5 +1,4 @@
 import click
-from prompt import Prompt
 import subprocess
 import mysql.connector
 from mysql.connector.errors import DatabaseError
@@ -7,23 +6,6 @@ import sys
 import platform
 from mysql.connector import errorcode
 
-def guineapig_print(value):
-	print(f"GUINEAPIG🐹: {value}")
-
-
-def check_tables_exist(cnx, table):
-	cursor = cnx.cursor()
-	cursor.execute("""
-		SELECT COUNT(*)
-        FROM information_schema.tables
-        WHERE table_name = "{}"
-	""".format(table))
-	if cursor.fetchone()[0] == 1:
-		cursor.close()
-		return True
-
-	cursor.close()
-	return False
 
 
 
@@ -33,6 +15,7 @@ def main():
 
 @main.command()
 def setupdb():
+	from . import utils
 	# check if mysql is installed
 	try:
 		output = subprocess.check_output(["mysql", "--version"]).decode("utf-8")
@@ -41,7 +24,7 @@ def setupdb():
 		try:
 			output = subprocess.check_output(["brew", "--version"]).decode("utf-8")
 			# install mysql
-			guineapig_print("Start installing mysql")
+			utils.guineapig_print("Start installing mysql")
 			output = subprocess.check_output(["brew", "install", "mysql"]).decode("utf-8")
 			if "brew services restart mysql" in output:
 				print("mysql is installed.")
@@ -55,9 +38,9 @@ def setupdb():
 	cursor = cnx.cursor()
 	try:
 		cursor.execute("CREATE DATABASE guineapig_db")
-		guineapig_print("Database `guineapig_db` is created.")
+		utils.guineapig_print("Database `guineapig_db` is created.")
 	except DatabaseError:
-		guineapig_print("Database `guineapig_db` already exists.")
+		utils.guineapig_print("Database `guineapig_db` already exists.")
 
 	# create tables in guineapig_db
 	TABLES = {}
@@ -72,7 +55,8 @@ def setupdb():
 		CREATE TABLE item (
 			item_id INTEGER NOT NULL AUTO_INCREMENT,
 			item_value DECIMAL(5, 2), # 999.99
-			category_id INTEGER,
+			category_id INTEGER NOT NULL,
+			memo TEXT,
 			FOREIGN KEY (category_id)
 				REFERENCES category(category_id),
 			PRIMARY KEY (item_id)
@@ -85,12 +69,12 @@ def setupdb():
 		sql = TABLES[table]
 		try: 
 			cursor.execute(sql)
-			guineapig_print(f"Table '{table}' is created.")
+			utils.guineapig_print(f"Table '{table}' is created.")
 		except mysql.connector.Error as err:
 			if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-				guineapig_print(f"Table '{table}' exists.")
+				utils.guineapig_print(f"Table '{table}' exists.")
 			else:
-				guineapig_print(err.msg)
+				utils.guineapig_print(err.msg)
 
 	cursor.close()
 	cnx.close()
@@ -101,17 +85,18 @@ def setupdb():
 
 @main.command()
 def shell():
+	from . import utils
 	# connect to the database if it can be connected
 	cnx = None
 	try:
 		cnx = mysql.connector.connect(user="root",database="guineapig_db")
 	except mysql.connector.Error as err:
 		if err.errno == errorcode.ER_BAD_DB_ERROR:
-			guineapig_print("Database `guineapig_db` does not exist.")
-			guineapig_print("Please run `guineapig setupdb`.")
+			utils.guineapig_print("Database `guineapig_db` does not exist.")
+			utils.guineapig_print("Please run `guineapig setupdb`.")
 			sys.exit()
 		elif err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-			guineapig_print("Could not access to `guineapig_db` as root.")
+			utils.guineapig_print("Could not access to `guineapig_db` as root.")
 			sys.exit()
 
 	cursor = cnx.cursor()
@@ -120,23 +105,24 @@ def shell():
 	try:
 		cursor.execute("USE guineapig_db")
 	except mysql.connector.Error as err:
-		guineapig_print("Database `guineapig_db` does not exist.")
-		guineapig_print("Please run `guineapig setupdb`.")
+		utils.guineapig_print("Database `guineapig_db` does not exist.")
+		utils.guineapig_print("Please run `guineapig setupdb`.")
 		sys.exit()
 
 	# check if tables exist
-	exist1 = check_tables_exist(cnx, "item")
-	exist2 = check_tables_exist(cnx, "category")
+	exist1 = utils.check_tables_exist(cnx, "item")
+	exist2 = utils.check_tables_exist(cnx, "category")
 	if exist1 and exist2:
 		pass
 	else:
-		guineapig_print("Tables do not exist.")
-		guineapig_print("Please run `guineapig setupdb`.")
+		utils.guineapig_print("Tables do not exist.")
+		utils.guineapig_print("Please run `guineapig setupdb`.")
 		sys.exit()
 
 	cursor.close()
 	cnx.close()
-	Prompt().cmdloop()
+	from . import prompt
+	prompt.Prompt().cmdloop()
 
 
 
@@ -144,7 +130,8 @@ def shell():
 
 if __name__ == "__main__":
 	if platform.system() != "Darwin":
-		print("guineapig is currently only for Mac users.")
+		from . import utils
+		utils.guineapig_print("guineapig is currently only for Mac users.")
 		sys.exit()
 	main()
 
